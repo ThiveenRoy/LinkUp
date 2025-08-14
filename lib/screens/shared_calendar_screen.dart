79fbdad374a4517ccde563bcc7465d2edd9fb30f
ThiveenRoy <roythiveen@gmail.com>
2025-08-14 09:05:32 +0800

    fix(shared-calendar): stop auto re-join, show creatorName, add day/month agenda

--- lib/screens/shared_calendar_screen.dart
+++ lib/screens/shared_calendar_screen.dart
@@ -741,8 +741,8 @@ class _SharedCalendarScreenState extends State<SharedCalendarScreen> {
     }
 
     bool allowEdit = data['allowEdit'] ?? false;
-    String editLink = 'localhost:5000/#/cal/${data['sharedLinkEdit']}';
-    String viewLink = 'localhost:5000/#/cal/${data['sharedLinkView']}';
+    String editLink = 'https://linkupcalendar.app/#/cal/${data['sharedLinkEdit']}';
+    String viewLink = 'https://linkupcalendar.app/#/cal/${data['sharedLinkView']}';
 
     final TextEditingController linkController =
         TextEditingController(text: allowEdit ? editLink : viewLink);
